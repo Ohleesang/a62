8fa8e6ee678b85024aa8ad5bb732c2580b39cf22
Ohleesang <jammmmed@gmail.com>
2023-12-04 12:33:46 +0900

    테스트 케이스실패(ayayeayayraya 아무래도, 그 이전값 저장에서 뭔가 문제가있는듯)

--- app/src/main/java/com/example/a62/Solution.kt
+++ app/src/main/java/com/example/a62/Solution.kt
@@ -15,26 +15,33 @@ class Solution {
         var list = babbling.toMutableList()
         var filteredList :List<String>
         var exFilteredList = List<String>(list.size,{""}).toMutableList()
+        // 이전 필터된 마지막 문자들
         var count = 0
-        //while인데.... 언제 멈춰야하지? -->filtered 가 더이상 되지않는경우?
-        while(count<=list.size) {
+        // while인데.... 언제 멈춰야하지?
+        // -->filtered 가 더이상 되지 않는경우?
+        while(true) {
             for (index in list.indices) {
-                //각 리스트에 diction 을 빼서 비어 있다면 count
+                //각 리스트에 diction 이 포함되어 있으면 걸러낸다.
                 filteredList = diction.filter { list[index].contains(it) }
 
                 //필터링된 리스트 값을 뺀다.
                 if (filteredList.isEmpty()) count++
                 else {
+
                     for (f in filteredList) {
                         //이전과 같으면 시행되면안된다!!
-                        if(exFilteredList[index]==f) continue
+                        if(exFilteredList[index]==f) list[index]="-1" //더이상 계산필요 x
 
-                        list[index] = list[index].substringAfter(f)
+                        list[index] = list[index].replaceFirst(f,"")
 
                     }
                     exFilteredList[index] = filteredList.last()
                 }
             }
+            if(count>=list.size) break
+
+            //값 초기화
+            count = 0
         }
 
 
@@ -48,6 +55,8 @@ class Solution {
 }
 fun main(){
     val a = Solution()
-    a.solution(arrayOf("aya", "yee", "u", "maa"))
-    a.solution(arrayOf("ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"))
+//    a.solution(arrayOf("aya", "yee", "u", "maa"))
+//    a.solution(arrayOf("ayaye", "uuu", "yeye", "yemawoo", "ayaayaa"))
+//    a.solution(arrayOf("maya"))
+    a.solution(arrayOf("ayayeayayeaya"))
 }
